4de17072278136cc9a849c70c344ca12f23bd0bd
Wátila Freire da Costa <watilafreire26@gmail.com>
2020-12-17 17:11:28 -0300

    fix database cleaner drop db development

--- spec/support/database_cleaner.rb
+++ spec/support/database_cleaner.rb
@@ -1,13 +1,14 @@
 RSpec.configure do |config|
+  if Rails.env.test?
+    config.before(:suite) do
+      DatabaseCleaner.strategy = :transaction
+      DatabaseCleaner.clean_with(:truncation)
+    end
 
-  config.before(:suite) do
-    DatabaseCleaner.strategy = :transaction
-    DatabaseCleaner.clean_with(:truncation)
-  end
-
-  config.around(:each) do |example|
-    DatabaseCleaner.cleaning do
-      example.run
+    config.around(:each) do |example|
+      DatabaseCleaner.cleaning do
+        example.run
+      end
     end
   end
 end
